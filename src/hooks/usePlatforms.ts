f5fdc75bb8ecaee942ea45a2749209747da14836
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-12 16:22:48 +0530

    added static platforms list

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,10 @@
-import useData from "./useData";
-
+import platforms from "../data/platforms";
 interface Platforms {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatform = () => useData<Platforms>("/platforms/lists/parents");
+const usePlatform = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatform;
