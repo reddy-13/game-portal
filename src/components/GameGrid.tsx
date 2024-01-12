daf2bc7f078e2b96e32243cf9a2c01c9ba858707
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-12 16:35:45 +0530

    Reafactor gam grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,10 +15,11 @@ const GameGrid = ({ gameQuery }: Props) => {
     const { data: games, error, isLoading } = UseGames(gameQuery);
 
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    if (error) return <Text>{error}</Text>;
 
     return (
         <>
-            {error && <Text>{error}</Text>}
+
             <SimpleGrid columns={{
                 sm: 1, md: 2, lg: 3, xl: 4
             }} spacing={6} padding={10}>
