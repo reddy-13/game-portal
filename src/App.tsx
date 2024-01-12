e4fc1259f346150080fabbb26d38c2810c0c7979
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-12 15:39:52 +0530

    impiment search

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -28,7 +29,7 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <Navbar />
+        <Navbar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,6 +23,7 @@ const UseGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
