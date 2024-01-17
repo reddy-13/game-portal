570e5c407c69e0f8bf1d23a62647bd92a54cc842
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-17 14:46:51 +0530

    add infinite load more button

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,10 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import { Button, SimpleGrid, Text, Box } from '@chakra-ui/react';
 import { GameQuery } from '../App';
 import UseGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
+import React from 'react';
 
 
 interface Props {
@@ -11,31 +12,37 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-    const { data, error, isLoading } = UseGames(gameQuery);
+    const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = UseGames(gameQuery);
 
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     if (error) return <Text>{error.message}</Text>;
 
     return (
-        <>
+        <Box padding={10}>
 
             <SimpleGrid columns={{
                 sm: 1, md: 2, lg: 3, xl: 4
-            }} spacing={6} padding={10}>
+            }} spacing={6} >
                 {isLoading && skeletons.map((skeleton) =>
                     <GameCardContainer key={skeleton}>
                         <GameCardSkeleton />
                     </GameCardContainer>
                 )}
+                {data?.pages.map((page, index) =>
+                    <React.Fragment key={index}>
+                        {page.results.map(game =>
+                            <GameCardContainer key={game.id} >
+                                <GameCard game={game} />
+                            </GameCardContainer>)}
+                    </React.Fragment>)}
 
-                {data?.results.map(game => (
-                    <GameCardContainer key={game.id} >
-                        <GameCard game={game} />
-                    </GameCardContainer>
-                ))}
 
             </SimpleGrid>
-        </>
+            {hasNextPage && (
+                <Button onClick={() => fetchNextPage()} marginY={10}>
+                    {isFetchingNextPage ? 'Loading...' : 'Load More'}
+                </Button>)}
+        </Box>
 
     )
 }
