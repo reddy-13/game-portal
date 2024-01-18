0b7e155c04912c219b0ecaeb287d174184b4431b
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-18 13:19:27 +0530

    fetch game and refectror entites

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-import { Game } from "./useGames";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
+
 const useGame = (slug: string) =>
   useQuery({
     queryKey: ["games", slug],
