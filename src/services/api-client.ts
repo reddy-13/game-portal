54fd780b025401d353bc825e64d5996bb1cf32c8
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-18 17:32:08 +0530

    env movidfied

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "be9ed16bd5d0443a8130cef4bd55fd4a",
+    key: import.meta.env.VITE_SECRET_KEY,
   },
 });
 
