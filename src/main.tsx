e4de4a004e6bc345879ac732f739a8ec1423a9f8
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-18 17:41:09 +0530

    addted vecel metrics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import theme from "./theme";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
@@ -17,6 +18,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <SpeedInsights />
+        <Analytics />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
