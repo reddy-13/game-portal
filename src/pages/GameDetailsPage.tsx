fe0d51be256a7f7a98e0c041e1e2342d7533fc6c
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-18 14:39:49 +0530

    fixed error

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import useGame from "../hooks/useGame"
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
+import { GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
 import ExpandableText from "../components/ExpandableText"
 import DefinationItem from "../components/DefinationItem"
 import CriticScrore from "../components/CriticScrore"
@@ -17,13 +17,24 @@ const GameDetailsPage = () => {
     if (error || !game) throw error;
 
     return (
-        <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-            <GameAttributes game={game} />
-            <GameTrailer gameId={game.id} />
-            <GameScreenshots gameId={game.id} />
-        </>
+        <SimpleGrid
+            columns={{
+                base: 1,
+                md: 2
+            }}
+            spacing={5}
+        >
+            <GridItem>
+                <Heading>{game.name}</Heading>
+                <ExpandableText>{game.description_raw}</ExpandableText>
+                <GameAttributes game={game} />
+
+            </GridItem>
+            <GridItem>
+                <GameTrailer gameId={game.id} />
+                <GameScreenshots gameId={game.id} />
+            </GridItem>
+        </SimpleGrid>
     )
 }
 
