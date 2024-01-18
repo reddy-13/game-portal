244660faa398dcef192702d4ad812ddc01fda202
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-18 14:39:34 +0530

    Refacort with fix

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,18 +1,24 @@
-import { Spinner } from "@chakra-ui/react"
-import useTrailers from "../hooks/useTrailers"
+import useTrailers from '../hooks/useTrailers';
 
 interface Props {
-    gameId: number
+    gameId: number;
 }
+
 const GameTrailer = ({ gameId }: Props) => {
-    const { data, error, isLoading } = useTrailers(gameId)
-    if (isLoading) return <Spinner />;
-    if (error) return error;
-    const first = data?.results[0]
-    return first ? <video
-        src={first.data[480]}
-        poster={first.preview}
-        controls
-    /> : null
-}
-export default GameTrailer
+    const { data, error, isLoading } = useTrailers(gameId);
+
+    if (isLoading) return null;
+
+    if (error) throw error;
+
+    const first = data?.results[0];
+    return first ? (
+        <video
+            src={first.data[480]}
+            poster={first.preview}
+            controls
+        />
+    ) : null;
+};
+
+export default GameTrailer;
